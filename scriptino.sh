c97c01d1da5c6c5d63b2b37fce8e5a790901466e
buonhobo <bnnalx@gmail.com>
2024-08-31 12:19:41 +0200

    added automated script for desktop

--- scriptino.sh
+++ scriptino.sh
@@ -2,6 +2,10 @@
 
 sudo ostree config set sysroot.bootloader none
 
+sudo tee -a /etc/rpm-ostreed.conf <<EOF
+AutomaticUpdatePolicy=stage
+EOF
+
 sudo rm /etc/yum.repos.d/{_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo,fedora-cisco-openh264.repo,google-chrome.repo,rpmfusion-nonfree*}
 sudo wget --output-document /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 rpm-ostree install cascadia-code-fonts ddcutil tailscale --apply-live
@@ -12,7 +16,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable --now flatpak-system-update.timer
 # sudo systemctl --user enable --now brew-update.timer
 
-systemctl enable --now tailscaled.service
+sudo systemctl enable --now tailscaled.service
 sudo tailscale up --operator=buonhobo
 
 sudo mkdir /home/shared
@@ -24,21 +28,24 @@ EOF
 
 echo "Please connect to Oneplus 7T..."
 read
-sudo rsync -rP etc/NetworkManager /etc/
 sudo nmcli connection modify OnePlus\ 7T ipv4.route-metric 99
 sudo nmcli connection modify enp39s0 802-3-ethernet.wake-on-lan magic
+sudo nmcli connection modify enp39s0 ipv4.dns 8.8.8.8,8.8.4.4
+sudo nmcli connection modify enp39s0 ipv4.ignore-auto-dns yes
 sudo systemctl restart NetworkManager systemd-resolved.service
 
 echo "Get ready to insert the private key in nano..."
 read
-nano .ssh/id_ed25519
-ssh-keygen -y -f .ssh/id_ed25519 > .ssh/id_ed25519.pub
-chmod 600 .ssh/id_ed25519*
+nano ~/.ssh/id_ed25519
+echo "Please insert the private key password..."
+ssh-keygen -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
+chmod 600 ~/.ssh/id_ed25519*
 
 curl -f https://zed.dev/install.sh | sh
 
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install com.github.flxzt.rnote \
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install flathub \
+    com.github.flxzt.rnote \
     com.github.iwalton3.jellyfin-media-player \
     com.github.tchx84.Flatseal \
     com.mattjakeman.ExtensionManager \
@@ -51,6 +58,6 @@ flatpak install com.github.flxzt.rnote \
     org.onlyoffice.desktopeditors \
     org.videolan.VLC \
     page.codeberg.libre_menu_editor.LibreMenuEditor
-flatpak uninstall org.gnome.Evince
+sudo flatpak uninstall org.gnome.Evince
 
-rpm-ostree override remove gnome-software gnome-software-rpm-ostree
+sudo rpm-ostree override remove gnome-software gnome-software-rpm-ostree
