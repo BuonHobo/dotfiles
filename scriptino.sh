4c42f1a7a0291972f736c0b5d60369f8344c79b3
buonhobo <bnnalx@gmail.com>
2024-11-03 19:36:21 +0100

    added muse bot

--- scriptino.sh
+++ scriptino.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo ostree config set sysroot.bootloader none
-
 sudo tee -a /etc/rpm-ostreed.conf <<EOF
 AutomaticUpdatePolicy=stage
 EOF
@@ -9,15 +7,15 @@ sudo systemctl enable --now rpm-ostreed-automatic.timer
 
 sudo rm /etc/yum.repos.d/{_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo,fedora-cisco-openh264.repo,google-chrome.repo,rpmfusion-nonfree*}
 sudo wget --output-document /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-rpm-ostree install cascadia-code-fonts ddcutil tailscale --apply-live
+rpm-ostree install ddcutil tailscale --apply-live
 
 sudo rsync ./etc/udev /etc/ -rP
 udevadm control --reload-rules && udevadm trigger
 
 rsync ./home/buonhobo /home/ -rP
-sudo rsync ./etc/systemd /etc/ -rP
-sudo systemctl daemon-reload
-sudo systemctl enable --now flatpak-system-update.timer
+# sudo rsync ./etc/systemd /etc/ -rP
+# sudo systemctl daemon-reload
+# sudo systemctl enable --now flatpak-system-update.timer
 # sudo systemctl --user enable --now brew-update.timer
 
 sudo systemctl enable --now tailscaled.service
@@ -37,6 +35,8 @@ sudo nmcli connection modify enp39s0 802-3-ethernet.wake-on-lan magic
 sudo nmcli connection modify enp39s0 ipv4.dns 8.8.8.8,8.8.4.4
 sudo nmcli connection modify enp39s0 ipv4.ignore-auto-dns yes
 sudo systemctl restart NetworkManager systemd-resolved.service
+sudo nmcli connection modify enp39s0 ipv6.method disabled
+sudo nmcli connection modify enp39s0 connection.dns-over-tls yes
 
 echo "Get ready to insert the private key in nano..."
 read
